78a0e83c61b336ccfc9a2606d92b777a1c57a5c0
Mskurre06 <mrkurre06@gmail.com>
2025-04-18 19:50:42 +0300

    Jakso 2

--- src/main/java/net/lucasmakinen/tutorialmod2/TutorialMod2.java
+++ src/main/java/net/lucasmakinen/tutorialmod2/TutorialMod2.java
@@ -2,6 +2,7 @@ package net.lucasmakinen.tutorialmod2;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.lucasmakinen.tutorialmod2.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 //:3
@@ -12,5 +13,6 @@ public class TutorialMod2 implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 	}
 }
