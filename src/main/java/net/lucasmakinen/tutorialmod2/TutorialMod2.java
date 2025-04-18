e42062bbb99d5ee9b5b0d0962194f18a9df0c819
Mskurre06 <mrkurre06@gmail.com>
2025-04-18 18:59:38 +0300

    juujuu

---
+++ src/main/java/net/lucasmakinen/tutorialmod2/TutorialMod2.java
@@ -0,0 +1,16 @@
+package net.lucasmakinen.tutorialmod2;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod2 implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod2";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
